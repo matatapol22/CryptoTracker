089b434f83efdd93d532e84e30dc6468ae16a986
matatapol22 <matatapol22@gmail.com>
2025-05-14 21:55:22 +0700

    Объект для конфигурации сети

---
+++ app/src/main/java/com/bignerdranch/android/cryptotracker/RetrofitInstance.kt
@@ -0,0 +1,17 @@
+package com.bignerdranch.android.cryptotracker
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitInstance {
+    private val retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl("https://api.coingecko.com/api/v3/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    val api: CoinGeckoApi by lazy {
+        retrofit.create(CoinGeckoApi::class.java)
+    }
+}
