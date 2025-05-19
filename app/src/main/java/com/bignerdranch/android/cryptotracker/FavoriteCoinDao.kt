4f50bec61a66401e34aac081fcc9788c71b90b46
matatapol22 <matatapol22@gmail.com>
2025-05-19 22:56:30 +0700

    Создание DAO

---
+++ app/src/main/java/com/bignerdranch/android/cryptotracker/FavoriteCoinDao.kt
@@ -0,0 +1,19 @@
+package com.bignerdranch.android.cryptotracker
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface FavoriteCoinDao {
+    @Query("SELECT * FROM favorite_coins")
+    suspend fun getAll(): List<FavoriteCoin>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(coin: FavoriteCoin)
+
+    @Delete
+    suspend fun delete(coin: FavoriteCoin)
+}
